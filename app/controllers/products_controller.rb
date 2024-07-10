584e2371410a9921088def16e5d9b03ba1f17404
phadhiil001 <phadhiil001@gmail.com>
2024-07-10 13:49:05 -0500

    Added more styling to the Homepage and the product show pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,22 @@
+# app/controllers/products_controller.rb
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    if params[:search].present?
+      Rails.logger.debug "Search query: #{params[:search]}"
+      @products = @products.where('name LIKE ? OR description LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%")
+    end
+
+    if params[:category].present?
+      Rails.logger.debug "Category filter: #{params[:category]}"
+      @products = @products.joins(:category).where(categories: { id: params[:category] })
+    end
+
+    @products = @products.page(params[:page]).per(10)
   end
 
   def show
-    @product = Product.find(params[:id])
+    @product = Product.find_by(id: params[:id])
   end
 end
