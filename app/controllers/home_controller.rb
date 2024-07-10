b54d8ef993f72c28e6107483b35628a14b1f5483
phadhiil001 <phadhiil001@gmail.com>
2024-07-10 11:22:24 -0500

    Created the Product and Category controller, and added devise for views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @categories = Category.all
+    @products = Product.all.limit(10)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_10_144136) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_10_161727) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -69,6 +69,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_10_144136) do
     t.string "name"
     t.text "description"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
