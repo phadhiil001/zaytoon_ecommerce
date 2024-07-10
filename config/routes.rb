9aca5b63268174fe09180a38469745c700ed6898
phadhiil001 <phadhiil001@gmail.com>
2024-07-10 11:13:32 -0500

    Added the controllers for Home, About and Contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  root 'home#index'
+
+  get 'about', to: 'static_pages#about'
+  resources :categories, only: [:index, :show]
+  resources :products, only: [:index, :show]
+  resource :contact, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
